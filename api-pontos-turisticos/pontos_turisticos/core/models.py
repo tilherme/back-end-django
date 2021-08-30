ff601f990c2468f02ffd8bb63d279973a6a7b5ed
tilherme <gui.mauricio@hotmail.com.br>
2021-08-30 18:58:12 -0300

    erro corrigido

--- api-pontos-turisticos/pontos_turisticos/core/models.py
+++ api-pontos-turisticos/pontos_turisticos/core/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from atracoes.models import Atracao
 class pontos_turisticos(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracao)
     def __str__(self):
         return self.nome
--- api-pontos-turisticos/pontos_turisticos/pontos_turisticos/settings.py
+++ api-pontos-turisticos/pontos_turisticos/pontos_turisticos/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core'
-    
-    
+    'core',
+    'atracoes',    
 ]
 
 MIDDLEWARE = [
